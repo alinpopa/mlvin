836f6e05a43bf229784a67be2c3b318574d912c0
Alin Popa <alin.popa@gmail.com>
2017-01-07 21:49:07 +0000

    Refactoring.

--- src/mlvin.ml
+++ src/mlvin.ml
@@ -4,16 +4,16 @@ open Cohttp_lwt_unix
 
 let display_body body =
   Printf.printf "Body of length: %d\n" (String.length body)
-  |> fun _ -> Printf.printf "Raw body: '%s'\n" body
-  |> fun _ -> body
+  |> fun () -> Printf.printf "Raw body: '%s'\n" body
+  |> fun () -> body
 
 let display_resp resp =
   resp
   |> Response.status
   |> Code.code_of_status
   |> fun code -> Printf.printf "Response code: %d\n" code
-  |> fun _ -> Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string)
-  |> fun _ -> resp
+  |> fun () -> Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string)
+  |> fun () -> resp
 
 let body =
   Client.get (Uri.of_string "https://api.ipify.org?format=json") >>= fun (resp, body) ->
