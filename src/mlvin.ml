6bb65a5c06914c18a614d288a98d3f9f1bbadb4d
Alin Popa <alin.popa@gmail.com>
2017-01-07 21:35:28 +0000

    Parse the ipify message.

--- src/mlvin.ml
+++ src/mlvin.ml
@@ -2,15 +2,28 @@ open Lwt
 open Cohttp
 open Cohttp_lwt_unix
 
+let display_body body =
+  Printf.printf "Body of length: %d\n" (String.length body);
+  Printf.printf "Raw body: '%s'\n" body;
+  body
+
+let display_resp resp =
+  let code = resp |> Response.status |> Code.code_of_status in
+  Printf.printf "Response code: %d\n" code;
+  Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
+  resp
+
 let body =
   Client.get (Uri.of_string "https://api.ipify.org?format=json") >>= fun (resp, body) ->
-    let code = resp |> Response.status |> Code.code_of_status in
-    Printf.printf "Response code: %d\n" code;
-    Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
-    body |> Cohttp_lwt_body.to_string >|= fun body ->
-      Printf.printf "Body of length: %d\n" (String.length body);
+    resp
+    |> display_resp
+    |> fun _ -> body
+    |> Cohttp_lwt_body.to_string >|= fun body ->
       body
+      |> display_body
+      |> Yojson.Basic.from_string
 
 let () =
   let body = Lwt_main.run body in
-  print_endline ("Receive body\n" ^ body)
+  let open Yojson.Basic.Util in
+  print_endline ("Ip address: " ^ (body |> member "ip" |> to_string))
