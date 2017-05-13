b7841e115c29007ba96975f906277ac78bbe2a31
Alin Popa <alin.popa@gmail.com>
2017-05-13 10:57:01 +0100

    Initial async implementation for ping-pong alive check.

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,3 @@
-open Lwt
 open Core.Std
 open Mlvin
 open Cmdliner
@@ -7,23 +6,57 @@ let token =
   let doc = "The Slack API access token" in
   Cmdliner.Arg.(required & opt (some string) None & info ["t"; "token"] ~docv:"TOKEN" ~doc)
 
-let privileged_user =
-  let doc = "The user ID that is allowed to talk to the bot" in
-  Cmdliner.Arg.(required & opt (some string) None & info ["u"; "user"] ~docv:"USER" ~doc)
-
 let info =
   let doc = "My own Slack Bot." in
   Cmdliner.Term.info "mlvin" ~doc
 
-let run token user =
-  "Token " ^ token ^ ", user " ^ user |> print_endline;
-  let rtm = Lwt_main.run (Slack.start_rtm token) in
-  let member = Yojson.Basic.Util.member in
-  let to_string = Yojson.Basic.Util.to_string in
-  Lwt_main.run @@ Slack.client @@ (Uri.with_scheme (Uri.of_string (rtm |> member "url" |> to_string)) (Option.some "https"))
+let rec restart_listener restart_r f =
+  let open Async.Std in
+  Pipe.read restart_r >>= (fun m ->
+    match m with
+    | `Ok "restart" ->
+        printf "Restarting listener\n";
+        f ();
+        restart_listener restart_r f
+    | _ ->
+        printf "Got other message\n";
+        restart_listener restart_r f)
+
+let rec feedback_loop feedback_r restart_w =
+  let open Async.Std in
+  Pipe.read feedback_r >>= (fun r ->
+    match r with
+    | `Ok Data.KillMeNow f ->
+        f ();
+        Pipe.write restart_w "restart" >>= (fun _ ->
+          feedback_loop feedback_r restart_w)
+    | `Ok Data.Simple x ->
+        printf "Got a simple msg: %s\n" x;
+        feedback_loop feedback_r restart_w
+    | `Eof ->
+        Deferred.return (printf "Eof; closing the feedback loop\n"))
+
+let run token =
+  let open Async.Std in
+  let (restart_r, restart_w) = Pipe.create () in
+  let (feedback_r, feedback_w) = Pipe.create () in
+  let f () =
+    let module Json = Yojson.Basic.Util in
+    let module Handler = Async.Std.Handler in
+    let handler = Handler.create (fun x -> printf "Url: %s\n" x) in
+    let rtm = Slack.start_rtm token in
+    let url_json = rtm >>| fun r -> Json.member "url" r in
+    let url = url_json >>| fun r -> Json.to_string r in
+    let _ = Handler.install handler url in
+    url >>| (fun url -> Slack.client (Uri.of_string url) feedback_w)
+  in
+  let _ = feedback_loop feedback_r restart_w in
+  let _ = restart_listener restart_r f in
+  let _ = Pipe.write restart_w "restart" in
+  never_returns (Scheduler.go ~raise_unhandled_exn:true ())
 
 let execute = Cmdliner.Term.(
-  pure run $ token $ privileged_user)
+  pure run $ token)
 
 let () =
   match Cmdliner.Term.eval (execute, info) with
--- src/slack.ml
+++ src/slack.ml
@@ -1,54 +1,60 @@
-open Lwt
-open Cohttp
-open Cohttp_lwt_unix
-open Websocket_lwt
-
-let display_body body =
-  Printf.printf "Body of length: %d\n" (String.length body)
-  |> fun () -> Printf.printf "Raw body: '%s'\n" body
-  |> fun () -> body
-
-let display_resp resp =
-  resp
-  |> Response.status
-  |> Code.code_of_status
-  |> fun code -> Printf.printf "Response code: %d\n" code
-  |> fun () -> Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string)
-  |> fun () -> resp
+open Core.Std
+open Async.Std
 
 let start_rtm token =
-  let parse_body body =
-    Cohttp_lwt_body.to_string body >|= fun body ->
-      body |> display_body |> Yojson.Basic.from_string
-  in
-  let parse_response (resp, body) =
-    resp |> display_resp |> fun _ -> body |> parse_body
-  in
-  Client.get (Uri.of_string ("https://slack.com/api/rtm.start?token=" ^ token)) >>= parse_response
+  let open Async.Std.Deferred.Infix in
+  let module Client = Cohttp_async.Client in
+  let module Body = Cohttp_async.Body in
+  let module Json = Yojson.Basic in
+  let http_get = Client.get (Uri.of_string ("https://slack.com/api/rtm.start?token=" ^ token)) in
+  let string_body = http_get >>= fun (response, body) -> Body.to_string body in
+  string_body >>| fun s -> Json.from_string s
 
-let client uri =
-  let open Frame in
-  let module C = Cohttp in
-  Resolver_lwt.resolve_uri ~uri Resolver_lwt_unix.system >>= fun endp ->
-    let default_ctx = Conduit_lwt_unix.default_ctx in
-    (Conduit_lwt_unix.endp_to_client ~ctx:default_ctx endp >>= fun client ->
-      with_connection ~ctx:default_ctx client uri) >>= fun (recv, send) ->
-        let react fr =
-          match fr.opcode with
-          | Opcode.Ping -> send @@ Frame.create ~opcode:Opcode.Pong ()
-          | Opcode.Close ->
-              (if String.length fr.content >= 2 then
-                send @@ Frame.create ~opcode:Opcode.Close
-                ~content:(String.sub fr.content 0 2) ()
-              else send @@ Frame.close 1000) >>= fun () ->
-                Lwt.fail Exit
-          | Opcode.Pong -> Lwt.return_unit
-          | Opcode.Text -> Lwt_io.printf "Got some text: %s\n" fr.content
-          | Opcode.Binary -> Lwt_io.printf "%s\n" fr.content
-          | _ ->
-              send @@ Frame.close 1002 >>= fun () ->
-                Lwt.fail Exit
-        in
-        let rec react_forever () = recv () >>= react >>= react_forever
-        in
-        react_forever ()
+let rec read_data r alive_w =
+  let open Data in
+  Pipe.read r >>= (fun data ->
+    match data with
+    | `Ok d ->
+        printf "Raw data: '%s'\n" d;
+        let _ = match Data.of_string d with
+        | Some Pong ->
+            Pipe.write_without_pushback alive_w "alive"
+        | Some Msg (m, u) -> printf "Got msg: %s, from user: %s\n" m u
+        | Some Other t -> printf "Other type: %s\n" t
+        | None -> printf "Invalid json message: %s\n" d in
+        read_data r alive_w
+    | `Eof ->
+        printf "Reader finished...\n";
+        Deferred.unit)
+
+let rec check_alive alive_r feedback_w s =
+  Clock.with_timeout (sec 10.0) (Pipe.read alive_r) >>= (fun r ->
+    match r with
+    | `Result _ ->
+        printf "Alive...\n";
+        check_alive alive_r feedback_w s
+    | `Timeout ->
+        let shutdown_sequence = fun () -> Async_extra.Import.Socket.(shutdown s `Both) in
+        Pipe.write feedback_w (Data.KillMeNow shutdown_sequence))
+
+let client uri feedback_w =
+  let host = Option.value_exn ~message:"no host in uri" Uri.(host uri) in
+  let port = Option.value ~default:443 Uri_services.(tcp_port_of_uri uri) in
+  let module W = Websocket_async in
+  let scheme = Option.value_exn ~message:"no scheme in uri" Uri.(scheme uri) in
+  let tcp_fun s r w =
+    Async_extra.Import.Socket.(setopt s Opt.nodelay true);
+    (if scheme = "https" || scheme = "wss"
+    then Conduit_async_ssl.ssl_connect r w
+    else Deferred.return (r, w)) >>= fun (r, w) ->
+      let module C = Cohttp in
+      let extra_headers = C.Header.init () in
+      let open Async.Std.Log.Global in
+      let r, w = W.client_ez ~extra_headers ~log:Lazy.(force log) ~heartbeat:Time_ns.Span.(of_int_sec 5) uri s r w in
+      let _ = Clock.every' ~continue_on_error:false (sec 9.0) (fun () ->
+        Pipe.write w (Data.to_json ({id = 100; type' = "ping"; channel = ""; user = ""}))) in
+      let (alive_r, alive_w) = Pipe.create () in
+      let _ = check_alive alive_r feedback_w s in
+      read_data r alive_w
+  in
+  Tcp.(with_connection (to_host_and_port host port) tcp_fun)
