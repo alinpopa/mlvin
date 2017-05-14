e939a4ec9bf9cff2010be753bb0dad4a38f56374
Alin Popa <alin.popa@gmail.com>
2017-05-14 10:47:32 +0100

    Cleanup

--- src/data.ml
+++ src/data.ml
@@ -28,20 +28,13 @@ module Event = struct
     in
     to_string json
 
-  let from_string_opt event =
-    let from_string = Yojson.Basic.from_string in
-    try
-      let json = from_string event in
-      Some json
-    with
-    Yojson.Json_error _ -> None
-
   let of_json event =
     let open Core.Std.Option in
     let member = Yojson.Basic.Util.member in
     let to_string_option = Yojson.Basic.Util.to_string_option in
     let to_int_option = Yojson.Basic.Util.to_int_option in
-    (from_string_opt event) >>= (fun json ->
+    let from_string = Yojson.Basic.from_string in
+    (try_with (fun () -> from_string event)) >>= (fun json ->
       (json |> member "type" |> to_string_option) >>= (fun type' ->
         let id = value (json |> member "id" |> to_int_option) ~default:0 in
         match type' with
